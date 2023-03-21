94d061ef7c7ed8848d156a4d6dda50aade69ee63
Uday Mandal,Software Engineer <58983568+UdayMandal@users.noreply.github.com>
2023-03-21 05:14:26 +0000

    Add files via upload

---
+++ src/main/java/com/codeudaykumar/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.codeudaykumar.repository;
+
+import com.codeudaykumar.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
